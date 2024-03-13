c5057d3be056e099d56afaa2a1af93a571be80c7
wildanniam <wildanniam4@gmail.com>
2024-03-13 13:15:16 +0700

    main program

--- kpl-jurnal-mod4/Program.cs
+++ kpl-jurnal-mod4/Program.cs
@@ -1,4 +1,6 @@
-﻿public class KodeBuah
+﻿using System.Threading.Channels;
+
+public class KodeBuah
 {
     public enum Buah
     {
@@ -10,6 +12,13 @@
     {
         string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
         return kodeBuah[(int)buah];
+    }
+    public static void Main(string[] args)
+    {
+        KodeBuah buah = new KodeBuah();
+        Console.WriteLine("nama buah = " + Buah.Apel);
+        Console.WriteLine("kode buah = " + getKodeBuah(Buah.Apel));
+
     }
 
 }
