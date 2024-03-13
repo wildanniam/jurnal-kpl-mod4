b897158d7b1b5e7203311585f474c6e3117439b9
wildanniam <wildanniam4@gmail.com>
2024-03-13 12:56:52 +0700

    Menambahkan class kode buah

---
+++ kpl-jurnal-mod4/Program.cs
@@ -0,0 +1,74 @@
+﻿public class KodeBuah
+{
+    public enum Buah
+    {
+        Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri,
+        Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka
+    }
+
+    public static string getKodeBuah(Buah buah)
+    {
+        string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+        return kodeBuah[(int)buah];
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
